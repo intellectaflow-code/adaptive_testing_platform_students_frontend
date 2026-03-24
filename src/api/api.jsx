08ac5da88d612ca4761b379a5a72f889c8edbce6
INTELLECTAFLOW LLP <intellectaflow@gmail.com>
2026-03-24 09:12:09 +0530

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://adaptive-quiz-api-service-759082157852.asia-south1.run.app/api/v1",
 });
 
 API.interceptors.request.use((config) => {
